b8fe9076b867e3f776651a852fe64c4d81b17bb7
Rainer Kuhn <ticktricktrack@gmail.com>
2024-11-22 22:21:45 +0000

    experiment to play stream with added captcha bot token

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # synth
 Discord Music Bot
+
+# obtain PO_TOKEN:
+run this project, comes already dockerized and spits out the captcha tokens
+`docker run quay.io/invidious/youtube-trusted-session-generator`
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import 'dotenv/config';
 import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { joinVoiceChannel, createAudioPlayer, createAudioResource } from '@discordjs/voice';
+import { YtdlCore, toPipeableStream } from '@ybd-project/ytdl-core';
 
 import { DisTube } from "distube";
-import { YouTubePlugin } from "@distube/youtube";
+// import { YouTubePlugin } from "@distube/youtube";
+
+const ytdl = new YtdlCore({
+  hl: 'en',
+  gl: 'US',
+})
 
 const client = new Client({
   intents: [
@@ -12,22 +19,6 @@ const client = new Client({
     GatewayIntentBits.GuildVoiceStates,
   ]
 });
-
-const distube = new DisTube(client, {
-  plugins: [new YouTubePlugin()],
-});
-
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
-
-client.on(Events.MessageCreate, message => {
-  console.log(`Message: ${message.content}`);
-  if (message.content === 'ping') {
-    message.reply('pong');
-  }
-});
-
 
 client.on(Events.InteractionCreate, async interaction => {
   if (!interaction.isCommand()) return;
@@ -40,17 +31,71 @@ client.on(Events.InteractionCreate, async interaction => {
 
   if (commandName === 'play') {
     console.log('Playing a song from YouTube.');
-    const voiceChannel = interaction.member.voice.channel;
-    const player = distube.voices
+    const channel = interaction.member.voice.channel;
     const song = interaction.options.getString("link", true);
     console.log(`Song: ${song}`);
 
-    player.join(voiceChannel);
-    player.get(interaction).setSelfDeaf(true);
+    const connection = joinVoiceChannel({
+      channelId: channel.id,
+      guildId: channel.guild.id,
+      adapterCreator: channel.guild.voiceAdapterCreator,
+    })
 
-    await distube.play(voiceChannel, song);
+    const player = createAudioPlayer();
+
+    const stream = await ytdl.download(song, {
+      filter: 'audioonly',
+      quality: 'highestaudio',
+      poToken: process.env.PO_TOKEN,
+      visitorData: process.env.VISITOR_DATA,
+    })
+    const pipeableStream = toPipeableStream(stream)
+    const resource = createAudioResource(pipeableStream);
+    connection.subscribe(player)
+    await player.play(resource);
     await interaction.reply('Playing a song from YouTube.');
   }
 });
+
+// const distube = new DisTube(client, {
+//   plugins: [new YouTubePlugin()],
+// });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on(Events.MessageCreate, message => {
+  console.log(`Message: ${message.content}`);
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
+});
+
+//Distube version - runs into bot confirmation issue
+
+// client.on(Events.InteractionCreate, async interaction => {
+//   if (!interaction.isCommand()) return;
+
+//   const { commandName } = interaction;
+
+//   if (commandName === 'ping') {
+//     await interaction.reply('Pong!');
+//   }
+
+//   if (commandName === 'play') {
+//     console.log('Playing a song from YouTube.');
+//     const voiceChannel = interaction.member.voice.channel;
+//     const player = distube.voices
+//     const song = interaction.options.getString("link", true);
+//     console.log(`Song: ${song}`);
+
+//     player.join(voiceChannel);
+//     player.get(interaction).setSelfDeaf(true);
+
+//     await distube.play(voiceChannel, song);
+//     await interaction.reply('Playing a song from YouTube.');
+//   }
+// });
 
 client.login(process.env.BOT_TOKEN);
